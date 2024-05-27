e7ea2fab0c85c10d16a5e8508c2ac735b5283dc3
ARES <sp42no@gmail.com>
2024-05-27 14:07:33 -0700

    Initial commit

---
+++ src/main/java/net/doku/eternalend/EternalEndClient.java
@@ -0,0 +1,10 @@
+package net.doku.eternalend;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class EternalEndClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
---
+++ src/main/java/net/doku/eternalend/EternalEndDataGenerator.java
@@ -0,0 +1,11 @@
+package net.doku.eternalend;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class EternalEndDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
